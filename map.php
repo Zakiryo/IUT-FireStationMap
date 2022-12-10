96a6727f6b194e0f79c529378e85fc6abb457d4a
Zakiryo <yohan.rudny@outlook.fr>
2022-12-10 20:57:42 +0100

    début du système d'itinéraire

--- map.php
+++ map.php
@@ -10,12 +10,11 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <title>Casernes de Paris</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -31,6 +30,11 @@ if (!isset($_SESSION['username'])) {
     <div id="map">
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
     <script src="script.js"></script>
 </body>
 
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const save = [];
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
 $(document).ready(() => {
+
     const stationSearch = document.querySelector('input');
     stationSearch.addEventListener('input', searchStation);
     initMap();
@@ -18,10 +19,11 @@ function initMap() {
         url: 'casernes.json',
         success: function (data) {
             $.each(data, function (i) {
-                let marker = L.marker(data[i].fields.geo_point_2d, alt = i)
-                    .bindPopup(`Secteur : ${data[i].fields.deno_cs}</br>
-                            Adresse : ${data[i].fields.adresse}`);
+                const marker = L.marker(data[i].fields.geo_point_2d);
                 stations.addLayer(marker);
+                marker.bindPopup(`Secteur : ${data[i].fields.deno_cs}</br>
+                                Adresse : ${data[i].fields.adresse}</br>
+                                <button type="button" class="btn btn-outline-dark" onclick="createRoute('${data[i].fields.geo_point_2d}');">S'y rendre</button>`);
             })
             stations.addTo(map);
         }
@@ -39,4 +41,17 @@ function searchStation(e) {
     save.forEach(element => {
         if (element.getPopup().getContent().includes(search.toUpperCase())) stations.addLayer(element)
     });
+}
+
+function createRoute(coords) {
+    L.Routing.control({
+        waypoints: [
+            L.latLng(48.93065947179935, 2.54744213526183),
+            L.latLng(coords.split(','))
+        ],
+        router: new L.Routing.osrmv1({
+            language: 'fr',
+            profile: 'car'
+        })
+    }).addTo(map);
 }
